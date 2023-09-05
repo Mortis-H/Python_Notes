ca6c69675f54590d9685d97897a85d76e743e873
Mortis.Huang <143809768+Mortis-H@users.noreply.github.com>
2023-09-05 17:52:41 +0800

    Update Pandas_examples.py

--- Pandas_examples.py
+++ Pandas_examples.py
@@ -2,3 +2,20 @@
 import pandas as pd
 df = pd.read_csv("input_name.csv", index_col=False)
 df.to_csv("output_name.csv", index=False)
+
+### 2. Using column names which include spaces
+import pandas as pd
+df = pd.read_csv("input_name.csv")
+df['New_Column'] = df['Age'].str[:1]
+
+### 3. Filter dataset like a PRO with QUERY method
+import pandas as pd
+df = pd.read_csv("input_name.csv")
+df.query('Year < 1992 and Time > 10')
+
+### 4. Query strings with(@ symbol) to easily reach variables
+import pandas as pd
+df = pd.read_csv("input_name.csv")
+min_year = 1992
+min_time = 10
+df.query('Year < @min_year and Time > @min_time')
