db24b89acb0dc0fbaf95a77c11e9976d3cd6ebdd
Mortis.Huang <143809768+Mortis-H@users.noreply.github.com>
2025-01-12 10:47:31 +0800

    Update 2D_mask.py

--- 2D_mask.py
+++ 2D_mask.py
@@ -1,5 +1,6 @@
 import cupy as cp
 import numpy as np
+import nvtx
 
 # 建立測試資料
 array_shape = (3, 400000)
@@ -8,16 +9,15 @@ data = cp.random.random(array_shape)  # 建立隨機測試資料
 # 建立一個 boolean mask (shape = 400000)
 mask = cp.random.random(400000) > 0.5  # 隨機產生 boolean mask
 
-# 方法1: 使用迴圈
-filtered_array1 = cp.zeros_like(data)
-for i in range(3):
-    filtered_array1[i,:][mask] = data[i,:][mask]
+for _ in range(5):
 
-# 方法2: 使用 take
-mask2 = cp.expand_dims(mask, axis=0)
-filtered_array2 = cp.where(cp.tile(mask2, (3, 1)), data, 0)
+    # 方法1: 使用迴圈
+    with nvtx.annotate("method1"):
+    filtered_array1 = cp.zeros_like(data)
+        for i in range(3):
+            filtered_array1[i,:][mask] = data[i,:][mask]
 
-# 比較兩個結果
-print("比較兩種方法的結果:")
-cp.testing.assert_array_equal(filtered_array1, filtered_array2)
-print("兩種方法結果完全相同!")
+    # 方法2: 使用 take
+    with nvtx.annotate("method2"):
+        mask2 = cp.expand_dims(mask, axis=0)
+        filtered_array2 = cp.where(cp.tile(mask2, (3, 1)), data, 0)
